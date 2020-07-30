65ad6fb2292fa0f5fa057d2bd9cb6b15fb0a3739
Simon Chapman <eatyourpeasapps@gmail.com>
2020-07-30 20:25:13 +0100

    alter file path save_as_excel

--- client_controllers/download_excel.py
+++ client_controllers/download_excel.py
@@ -1,11 +1,12 @@
 import json
 import pandas as pd
-from pathlib import Path
+from os import path
 import pandas as pd
 
 def save_as_excel(json_string: str, upload_root: str):
     data_list = json.loads(json_string)
     data = []
+    print(upload_root)
     for counter, value in enumerate(data_list):
         birth_date = value['birth_data']['birth_date']
         measurement_date = value['measurement_dates']['observation_date']
@@ -24,6 +25,6 @@ def save_as_excel(json_string: str, upload_root: str):
     headings = ['birth_date', 'measurement_date', 'gestation_weeks', 'gestation_days', 'estimated_date_delivery', 'corrected_decimal_age', 'chronological_decimal_age', 'measurement_value', 'measurement_method', 'sds', 'centile']
     data_frame = pd.DataFrame(data, columns=headings)
     # out_file_2 = Path.cwd().joinpath("static").joinpath('uploaded_data').joinpath("output.xlsx")
-    out_file_2 = Path.joinpath(upload_root, "output.xlsx")
+    out_file_2 = path.join(upload_root, "output.xlsx")
     excel_file = data_frame.to_excel(out_file_2)
     return excel_file
